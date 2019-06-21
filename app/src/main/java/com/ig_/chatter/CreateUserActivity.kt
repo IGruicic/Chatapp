6949c55bd7a3069a5da6c5a149351256871f01af
Ina Gruičić <inna.gruicic@gmail.com>
2019-06-21 16:19:16 +0200

    lesson 03

---
+++ app/src/main/java/com/ig_/chatter/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.ig_.chatter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatarClc(view: View){
+
+    }
+
+    fun generateColorClc(view: View){
+
+    }
+
+    fun createUserClc(view: View){
+
+    }
+}
