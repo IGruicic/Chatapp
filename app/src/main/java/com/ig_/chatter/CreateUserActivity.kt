1e7fb201e0ad48a2e8ad5530671754521eb7b570
Ina Gruičić <inna.gruicic@gmail.com>
2019-06-21 16:43:34 +0200

    lesson 4

--- app/src/main/java/com/ig_/chatter/CreateUserActivity.kt
+++ app/src/main/java/com/ig_/chatter/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.ig_.chatter
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import kotlin.random.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,10 +18,31 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatarClc(view: View){
+         val random = Random
+         val color = random.nextInt(2)
+         val avatar = random.nextInt(28)
 
+        if(color == 0){
+            userAvatar = "light$avatar"
+        }else{
+            userAvatar = "dark$avatar"
+        }
+        val resourceID = resources.getIdentifier(userAvatar, "drawable",packageName)
+        CUavatarImg.setImageResource(resourceID)
     }
 
     fun generateColorClc(view: View){
+       val random = Random
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        CUavatarImg.setBackgroundColor(Color.rgb(r,g,b))
+        val savedR = r.toDouble() /255
+        val savedG = g.toDouble() /255
+        val savedB = b.toDouble() /255
+
+        avatarColor = "[$savedR, $savedG, $savedB,1]"
 
     }
 
